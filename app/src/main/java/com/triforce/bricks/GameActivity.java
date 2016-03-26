d0a69eda8ded2cac51f0a112d0c81cb140ff9adc
boehis <sboehi@me.com>
2016-03-26 17:04:11 +0100

    Test

--- app/src/main/java/com/triforce/bricks/GameActivity.java
+++ app/src/main/java/com/triforce/bricks/GameActivity.java
@@ -12,6 +12,6 @@ public class GameActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
+        //Start game
     }
 }
