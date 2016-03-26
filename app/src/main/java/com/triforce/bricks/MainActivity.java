5f13208f41e21983937d768fc007cc8a3499f8a5
boehis <sboehi@me.com>
2016-03-26 16:32:44 +0100

    Test

--- app/src/main/java/com/triforce/bricks/MainActivity.java
+++ app/src/main/java/com/triforce/bricks/MainActivity.java
@@ -1,13 +1,29 @@
 package com.triforce.bricks;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStart = (Button)findViewById(R.id.btnStart);
+        final Context myContext = this;
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(myContext, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
